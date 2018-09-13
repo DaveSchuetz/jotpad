014897613ce660582bec34c422a7e7240d62034a
David Schuetz <38507773+DaveSchuetz@users.noreply.github.com>
2018-09-13 14:42:13 -0400

    touch ups

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Link, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Note from './Note'
 import Notes from './Notes'
 import * as routes from '../constants/routes'
